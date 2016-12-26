0f961f6c599b0109f273c1e1950fbb9ee159ab07
lzys90 <leongyzs@gmail.com>
2016-12-26 23:26:16 +0800

    Refactor to revealing module pattern

--- js/main.js
+++ js/main.js
@@ -1,40 +1,47 @@
-(function() {
-    var people = {
-        people: ['Will', 'Laura'],
-        init: function() {
-            this.cacheDOM(); // i.e. people.cacheDOM
-            this.bindEvents(); // i.e. people.bindEvents
-            this.render(); // i.e. people.render
-        },
-        cacheDOM: function() {
-            this.$el = $('#peopleModule'); // good practice to prepend $ to indicate jquery obj searches
-            this.$button = this.$el.find('button');
-            this.$input = this.$el.find('input');
-            this.$ul = this.$el.find('ul');
-            this.template = this.$el.find('#people-template').html(); // no $ because its just going to be a html string
-        },
-        bindEvents: function() { // only responsible for binding events
-            this.$button.on('click', this.addPerson.bind(this)); // context of 'this' will change to addPerson. so must rebind to people module.
-            this.$ul.delegate('span.del', 'click', this.deletePerson.bind(this)); // delegate: Attach a handler to one or more events for all elements that match the selector, now or in the future, based on a specific set of root elements.
-        },
-        render: function() { // only render methods interacts with html
-            var data = {
-                people: this.people
-            };
-            this.$ul.html(Mustache.render(this.template, data));
-        },
-        addPerson: function() {
-            this.people.push(this.$input.val());
-            this.render();
-            this.$input.val('');
-        },
-        deletePerson: function(event) {
+// in JS, everything wrapped in parenthesis gets evaluated, then you can execute it right away. i.e. (function(name){//some code...})("Will"). No need for init function because IIFE is immediately executed
+
+var people = (function() {
+    var people = ['Will', 'Laura'];
+
+    // cache DOM
+    var $el = $('#peopleModule');
+    var $button = $el.find('button');
+    var $input = $el.find('input');
+    var $ul = $el.find('ul');
+    var template = $el.find('#people-template').html();
+
+    // bind events
+    $button.on('click', addPerson);
+    $ul.delegate('span.del', 'click', deletePerson);
+
+    _render(); // use _ to indicate private methods
+
+    function _render() {
+        $ul.html(Mustache.render(template, {people: people}));
+    }
+
+    function addPerson(value) { // expose as API for other methods to call i.e. people.addPerson('Jasmine')
+        var name = (typeof value === "string") ? value : $input.val();
+        people.push(name);
+        _render();
+        $input.val('');
+    }
+
+    function deletePerson(event) { // expose as API for other methods to call i.e. people.deletePerson(1)
+        var i;
+        if (typeof event === "number") {
+            i = event;
+        } else {
             var $remove = $(event.target).closest('li');
-            var i = this.$ul.find('li').index($remove); // find the index of closest li
-            this.people.splice(i, 1);
-            this.render();
+            var i = $ul.find('li').index($remove);
         }
+        people.splice(i, 1);
+        _render();
+    }
+
+    return {
+        addPerson: addPerson,
+        deletePerson: deletePerson
     };
 
-    people.init();
-})()
+})();
